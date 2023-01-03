2d005f2ba03190bfca9618318fd4a39c8ef8c373
Michał Murawski <m.a.murawski@wp.pl>
2023-01-03 12:31:38 +0100

    do delete contact

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -19,9 +19,15 @@ const contactsSlice = createSlice({
                     }
                 }
             }
+        },
+        deleteContact: {
+            reducer(state, action) {
+                const index = state.findIndex(contact => contact.id === action.payload);
+                state.splice(index, 1);
+            },
         }
     }
 })
 
-export const { addContact } = contactsSlice.actions;
+export const { addContact, deleteContact } = contactsSlice.actions;
 export const contactsReducer = contactsSlice.reducer;
